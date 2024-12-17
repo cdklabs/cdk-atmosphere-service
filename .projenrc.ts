639d1ccede9321dc21778f21d7a35c111095d93c
Eli Polonsky <Eli.polonsky@gmail.com>
2024-12-17 09:26:34 +0200

    chore(dependencies): upgrade `aws-cdk-lib` (#4)
    Manual upgrade to align with current latest version
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,9 +2,12 @@ import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
 const project = new CdklabsConstructLibrary({
   author: 'AWS',
   authorAddress: 'aws-cdk-dev@amazon.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.173.1',
   defaultReleaseBranch: 'main',
-  devDeps: ['cdklabs-projen-project-types'],
+  devDeps: [
+    'cdklabs-projen-project-types',
+    'aws-cdk',
+  ],
   name: '@cdklabs/cdk-atmosphere-service',
   projenrcTs: true,
   release: false,
