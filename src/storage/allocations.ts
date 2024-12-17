c4027cb0c0b41bec7568b2ef94f0f17337e325e6
Eli Polonsky <Eli.polonsky@gmail.com>
2024-12-17 15:13:28 +0200

    feat: storage layer (#17)
    Resolves https://github.com/cdklabs/cdk-atmosphere-service/issues/7
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: Polonsky <epolon@bcd07455aba8.ant.amazon.com>
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ src/storage/allocations.ts
@@ -0,0 +1,32 @@
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+/**
+ * Allocations table.
+ */
+export class Allocations extends Construct {
+
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
+      pointInTimeRecovery: true,
+      timeToLiveAttribute: 'ttl',
+    });
+
+    // According to the design doc, secondary indexes are not needed
+    // because table scans are not performed and updates always use the primary key
+  }
+
+  public grantReadWrite(identity: iam.IGrantable) {
+    this.table.grantReadWriteData(identity);
+  }
+
+}
---
+++ src/storage/environments.ts
@@ -0,0 +1,32 @@
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+/**
+ * Environments table.
+ */
+export class Environments extends Construct {
+
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'account', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'region', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
+      pointInTimeRecovery: true,
+    });
+
+    // According to the design doc, no secondary indexes are needed
+    // because all access is done using the primary key (account + region)
+  }
+
+  public grantReadWrite(identity: iam.IGrantable) {
+    this.table.grantReadWriteData(identity);
+  }
+
+}
