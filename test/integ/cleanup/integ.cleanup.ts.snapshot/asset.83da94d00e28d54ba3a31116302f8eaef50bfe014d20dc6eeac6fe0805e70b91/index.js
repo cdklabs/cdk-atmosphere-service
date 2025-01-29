5ba98a45711569315cb78a328cce4d2e2b1c9550
cdklabs-automation <90142015+cdklabs-automation@users.noreply.github.com>
2025-01-29 13:53:37 -0500

    chore(deps): upgrade dev dependencies (#65)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]:
    https://github.com/cdklabs/cdk-atmosphere-service/actions/runs/13021808872
    
    ------
    
    *Automatically created by projen via the "upgrade-dev-deps-main"
    workflow*
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: iliapolo <eli.polonsky@gmail.com>

--- test/integ/cleanup/integ.cleanup.ts.snapshot/asset.83da94d00e28d54ba3a31116302f8eaef50bfe014d20dc6eeac6fe0805e70b91/index.js
+++ test/integ/cleanup/integ.cleanup.ts.snapshot/asset.83da94d00e28d54ba3a31116302f8eaef50bfe014d20dc6eeac6fe0805e70b91/index.js
@@ -3450,6 +3450,7 @@ var require_fs = __commonJS({
       "chown",
       "close",
       "copyFile",
+      "cp",
       "fchmod",
       "fchown",
       "fdatasync",
@@ -3457,8 +3458,10 @@ var require_fs = __commonJS({
       "fsync",
       "ftruncate",
       "futimes",
+      "glob",
       "lchmod",
       "lchown",
+      "lutimes",
       "link",
       "lstat",
       "mkdir",
@@ -3473,6 +3476,7 @@ var require_fs = __commonJS({
       "rm",
       "rmdir",
       "stat",
+      "statfs",
       "symlink",
       "truncate",
       "unlink",
@@ -3875,15 +3879,21 @@ var require_copy = __commonJS({
       if (!destStat) {
         await fs2.mkdir(dest);
       }
-      const items = await fs2.readdir(src);
-      await Promise.all(items.map(async (item) => {
-        const srcItem = path2.join(src, item);
-        const destItem = path2.join(dest, item);
-        const include = await runFilter(srcItem, destItem, opts);
-        if (!include) return;
-        const { destStat: destStat2 } = await stat.checkPaths(srcItem, destItem, "copy", opts);
-        return getStatsAndPerformCopy(destStat2, srcItem, destItem, opts);
-      }));
+      const promises = [];
+      for await (const item of await fs2.opendir(src)) {
+        const srcItem = path2.join(src, item.name);
+        const destItem = path2.join(dest, item.name);
+        promises.push(
+          runFilter(srcItem, destItem, opts).then((include) => {
+            if (include) {
+              return stat.checkPaths(srcItem, destItem, "copy", opts).then(({ destStat: destStat2 }) => {
+                return getStatsAndPerformCopy(destStat2, srcItem, destItem, opts);
+              });
+            }
+          })
+        );
+      }
+      await Promise.all(promises);
       if (!destStat) {
         await fs2.chmod(dest, srcStat.mode);
       }
@@ -4003,7 +4013,15 @@ var require_copy_sync = __commonJS({
       return setDestMode(dest, srcMode);
     }
     function copyDir(src, dest, opts) {
-      fs2.readdirSync(src).forEach((item) => copyDirItem(item, src, dest, opts));
+      const dir = fs2.opendirSync(src);
+      try {
+        let dirent;
+        while ((dirent = dir.readSync()) !== null) {
+          copyDirItem(dirent.name, src, dest, opts);
+        }
+      } finally {
+        dir.closeSync();
+      }
     }
     function copyDirItem(item, src, dest, opts) {
       const srcItem = path2.join(src, item);
@@ -7316,7 +7334,7 @@ var require_finally = __commonJS({
       function succeed() {
         return finallyHandler.call(this, this.promise._target()._settledValue());
       }
-      function fail(reason) {
+      function fail2(reason) {
         if (checkCancel(this, reason)) return;
         errorObj2.e = reason;
         return errorObj2;
@@ -7347,7 +7365,7 @@ var require_finally = __commonJS({
               }
               return maybePromise._then(
                 succeed,
-                fail,
+                fail2,
                 void 0,
                 this,
                 void 0
@@ -7364,11 +7382,11 @@ var require_finally = __commonJS({
           return reasonOrValue;
         }
       }
-      Promise2.prototype._passThrough = function(handler7, type, success2, fail2) {
+      Promise2.prototype._passThrough = function(handler7, type, success2, fail3) {
         if (typeof handler7 !== "function") return this.then();
         return this._then(
           success2,
-          fail2,
+          fail3,
           void 0,
           new PassThroughHandlerContext(this, type, handler7),
           void 0
@@ -10774,13 +10792,14 @@ var require_unzip2 = __commonJS({
   }
 });
 
-// test/integ/allocate/assert.lambda.ts
+// test/integ/cleanup/assert.lambda.ts
 var assert_lambda_exports = {};
 __export(assert_lambda_exports, {
   handler: () => handler6
 });
 module.exports = __toCommonJS(assert_lambda_exports);
 var assert2 = __toESM(require("assert"));
+var import_client_s33 = require("@aws-sdk/client-s3");
 
 // src/cleanup/cleanup.client.ts
 var import_client_ecs = require("@aws-sdk/client-ecs");
@@ -12278,26 +12297,80 @@ var Runner = class _Runner {
   }
 };
 
-// test/integ/allocate/assert.lambda.ts
+// test/integ/cleanup/assert.lambda.ts
 var clients6 = RuntimeClients.getOrCreate();
 async function handler6(_) {
-  await Runner.assert("creates-the-right-resources", async (session) => {
+  await Runner.assert("deletes-stack-and-releases-environment", async (session) => {
     const response = await session.runtime.allocate({ pool: "release", requester: "test" });
-    assert2.strictEqual(response.status, 200);
     const body = JSON.parse(response.body);
-    const environment = await clients6.environments.get(body.environment.account, body.environment.region);
-    assert2.strictEqual(environment.status, "in-use");
-    assert2.strictEqual(environment.allocation, body.id);
-    const allocation = await clients6.allocations.get(environment.allocation);
-    assert2.strictEqual(allocation.account, body.environment.account);
-    assert2.strictEqual(allocation.region, body.environment.region);
-    const timeoutSchedule = await session.fetchAllocationTimeoutSchedule(body.id);
-    assert2.ok(timeoutSchedule);
+    const account = body.environment.account;
+    const region = body.environment.region;
+    const [stackName] = await session.deployStack({ templatePath: "cleanup/stacks/simple.yaml", region });
+    await clients6.environments.cleaning(body.id, account, region);
+    await session.runtime.cleanup({ allocationId: body.id, timeoutSeconds: 30 });
+    try {
+      await clients6.environments.get(account, region);
+      assert2.fail("expected environment to be deleted");
+    } catch (err) {
+      assert2.strictEqual(err.constructor.name, "EnvironmentNotFound");
+    }
+    const stack = await session.fetchStack(stackName, region);
+    assert2.ok(!stack);
   });
-  await Runner.assert("responds-with-locked-when-no-environments-are-available", async (session) => {
-    await session.runtime.allocate({ pool: "release", requester: "test" });
+  await Runner.assert("empties-and-deletes-buckets", async (session) => {
     const response = await session.runtime.allocate({ pool: "release", requester: "test" });
-    assert2.strictEqual(response.status, 423);
+    const body = JSON.parse(response.body);
+    const account = body.environment.account;
+    const region = body.environment.region;
+    const [stackName, resources] = await session.deployStack({ templatePath: "cleanup/stacks/versioned-bucket.yaml", region });
+    const bucketName = resources.filter((r) => r.ResourceType === "AWS::S3::Bucket").map((r) => r.PhysicalResourceId)[0];
+    const s33 = new import_client_s33.S3({ region });
+    await s33.putObject({ Bucket: bucketName, Key: "one.txt", Body: "one" });
+    await s33.putObject({ Bucket: bucketName, Key: "two.txt", Body: "two" });
+    await s33.deleteObject({ Bucket: bucketName, Key: "two.txt" });
+    await clients6.environments.cleaning(body.id, account, region);
+    await session.runtime.cleanup({ allocationId: body.id, timeoutSeconds: 60 });
+    const stack = await session.fetchStack(stackName, region);
+    assert2.ok(!stack);
+    try {
+      await s33.headBucket({ Bucket: bucketName });
+    } catch (err) {
+      assert2.strictEqual(err.name, "NotFound");
+    }
+  });
+  await Runner.assert("disables-termination-protection", async (session) => {
+    const response = await session.runtime.allocate({ pool: "release", requester: "test" });
+    const body = JSON.parse(response.body);
+    const account = body.environment.account;
+    const region = body.environment.region;
+    const [stackName] = await session.deployStack({ templatePath: "cleanup/stacks/simple.yaml", region, terminationProtection: true });
+    await clients6.environments.cleaning(body.id, account, region);
+    await session.runtime.cleanup({ allocationId: body.id, timeoutSeconds: 30 });
+    const stack = await session.fetchStack(stackName, region);
+    assert2.ok(!stack);
+  });
+  await Runner.assert("doesnt-release-a-dirty-environment", async (session) => {
+    const response = await session.runtime.allocate({ pool: "release", requester: "test" });
+    const body = JSON.parse(response.body);
+    const account = body.environment.account;
+    const region = body.environment.region;
+    await clients6.allocations.end({ id: body.id, outcome: "success" });
+    await clients6.environments.dirty(body.id, account, region);
+    await session.runtime.cleanup({ allocationId: body.id, timeoutSeconds: 30 });
+    const environment = await clients6.environments.get(account, region);
+    assert2.strictEqual(environment.status, "dirty");
+  });
+  await Runner.assert("marks-environment-dirty-if-fail", async (session) => {
+    const response = await session.runtime.allocate({ pool: "release", requester: "test" });
+    const body = JSON.parse(response.body);
+    const account = body.environment.account;
+    const region = body.environment.region;
+    const [stackName] = await session.deployStack({ templatePath: "cleanup/stacks/cannot-delete.yaml", region });
+    await clients6.environments.cleaning(body.id, account, region);
+    await session.runtime.cleanup({ allocationId: body.id, timeoutSeconds: 30 });
+    const environment = await clients6.environments.get(account, region);
+    assert2.strictEqual(environment.status, "dirty");
+    await session.destroyStack({ stackName, region });
   });
   return SUCCESS_PAYLOAD;
 }
